6a5449d81e9c1f4809d623fa44d6cb0f0a7139c5
Argemiro <argemiro.pcosta@gmail.com>
2021-04-21 13:15:44 -0300

    - Inicializando o repositório; - Adicionando os testes de health check.

---
+++ tests/pagamento/pagamento-health_check.js
@@ -0,0 +1,27 @@
+import http from 'k6/http'
+import {check} from 'k6'
+import encoding from "k6/encoding";
+
+export let options = {
+    vus: 10,
+    duration: '10s'
+}
+
+const username = 'usuario1';
+const password = 'senha1';
+
+export default function (){
+
+    const credentials = `${username}:${password}`;
+
+    const url = "http://localhost:8080/pagamento/servidor";
+
+    let response = http.get(url,
+        {headers: {"Authorization": "Basic " + encoding.b64encode(credentials)}});
+
+    check(response, {
+        "status is 200": (r) => r.status === 200,
+        "is correct message:": (r) => r.body === "Servidor no ar"
+    });
+
+}
